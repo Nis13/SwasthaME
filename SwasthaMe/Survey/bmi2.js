8293b7b336eee0b61ce3d9ac8f9626a56babe83a
Nistha Bajracharya <64536325+Nis13@users.noreply.github.com>
2023-02-12 06:02:34 +0545

    Add files via upload

---
+++ SwasthaMe/Survey/bmi2.js
@@ -0,0 +1,41 @@
+// document.getElementById("btn").addEventListener("click", function() {
+//    let age = parseInt(document.getElementById("age").value);
+//    let weight = parseFloat(document.getElementById("weight").value);
+//    let height = parseFloat(document.getElementById("height").value);
+
+//    let bmi = weight / (height * height);
+//    let result = "";
+
+//    if (bmi < 18.5) {
+//      result = "Underweight";
+//    } else if (bmi >= 18.5 && bmi < 25) {
+//      result = "Normal weight";
+//    } else if (bmi >= 25 && bmi < 30) {
+//      result = "Overweight";
+//    } else {
+//      result = "Obesity";
+//    }
+
+//    document.getElementById("result").innerHTML = "BMI: " + bmi + "<br />" + result;
+//  });
+
+//  document.getElementById("calculateBtn").addEventListener("click", function() {
+//    let age = parseInt(document.getElementById("age").value);
+//    let weight = parseFloat(document.getElementById("weight").value);
+//    let height = parseFloat(document.getElementById("height").value);
+
+//    let bmi = weight / (height * height);
+//    let result = "";
+
+//    if (bmi < 18.5) {
+//      result = "Underweight";
+//    } else if (bmi >= 18.5 && bmi < 25) {
+//      result = "Normal weight";
+//    } else if (bmi >= 25 && bmi < 30) {
+//      result = "Overweight";
+//    } else {
+//      result = "Obesity";
+//    }
+
+//    document.getElementById("result").innerHTML = "BMI: " + bmi + "<br />" + result;
+//  });
